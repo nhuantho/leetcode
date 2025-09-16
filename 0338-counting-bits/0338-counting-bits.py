05492ec8ec4ee26c2e98591e68f8a042822a93e8
nhuantho <91556274+nhuantho@users.noreply.github.com>
2025-09-16 09:34:50 +0700

    Time: 27 ms (27.03%), Space: 23.2 MB (5.13%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -5,7 +5,7 @@ class Solution:
         if n == 1:
             return [0, 1]
         res = []
-
+        visited = {}
         def generate_binary(n):
             if n == 0:
                 return 0
@@ -15,8 +15,13 @@ class Solution:
             while (n >= 1):
                 tmp_res = tmp_res + n % 2
                 n = n // 2
+                if n in visited:
+                    tmp_res = tmp_res + visited[n]
+                    break
             return tmp_res
 
         for i in range(0, n + 1):
-            res.append(generate_binary(i))
+            res_i = generate_binary(i)
+            visited.update({i: res_i})
+            res.append(res_i)
         return res
