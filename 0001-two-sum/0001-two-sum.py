b7173352783ef6e9864445b97b456bdb1265f885
nhuantho <91556274+nhuantho@users.noreply.github.com>
2025-09-18 08:59:43 +0700

    Time: 0 ms (100%), Space: 19 MB (23.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        if len(nums) == 0:
+        n = len(nums)
+        if n == 0:
             return []
-        dict_nums = {}
-
-        for i in range(len(nums)):
-            if target - nums[i] in dict_nums:
-                return [dict_nums[target - nums[i]], i]
-            dict_nums.update({nums[i]: i})
-
+        visited = dict()
+        for i in range(n):
+            if (target - nums[i]) in visited:
+                return [visited[target - nums[i]], i]
+            visited.update({nums[i]: i})
         return []
