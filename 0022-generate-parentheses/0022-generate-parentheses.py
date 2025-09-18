8c593170f38bf11772a57b6f47f5655eb639958f
nhuantho <91556274+nhuantho@users.noreply.github.com>
2025-09-18 23:45:59 +0700

    Time: 0 ms (100%), Space: 18.2 MB (5.68%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -2,43 +2,14 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         parenthesis_gen = []
 
-        def dfs(parenthesis_unit: List[str]):
+        def backtracking(parenthesis_unit: str, l, r):
             if len(parenthesis_unit) == n * 2:
-                parenthesis_gen.append(parenthesis_unit[:])
+                parenthesis_gen.append(parenthesis_unit)
                 return
+            if l < n:
+                backtracking(parenthesis_unit + '(', l + 1, r)
+            if r < l:
+                backtracking(parenthesis_unit + ')', l, r + 1)
 
-            parenthesis_unit.append("(")
-            dfs(parenthesis_unit)
-            parenthesis_unit.pop()
-
-            parenthesis_unit.append(")")
-            dfs(parenthesis_unit)
-            parenthesis_unit.pop()
-
-        def check_pair_parenthesis(parenthesis_unit: List[str]):
-            checkpoint = []
-            while parenthesis_unit:
-                tmp_p = parenthesis_unit.pop()
-                if tmp_p == "(" and len(checkpoint) == 0:
-                    return False
-                elif tmp_p == "(":
-                    tmp_c = checkpoint.pop()
-                    if tmp_c == "(":
-                        return False
-                    else:
-                        if len(checkpoint) == 0 and len(parenthesis_unit) == 0:
-                            return True
-                else:
-                    checkpoint.append(tmp_p)
-            return False
-
-        dfs([])
-        print(parenthesis_gen)
-
-        res = []
-
-        for item in parenthesis_gen:
-            tmp = item[:]
-            if check_pair_parenthesis(tmp):
-                res.append("".join(item[:]))
-        return res
+        backtracking('', 0, 0)
+        return parenthesis_gen
