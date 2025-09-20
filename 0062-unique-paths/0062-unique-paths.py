87c9afa89a32f05cd0a6ee851b1075b8da0f361a
nhuantho <91556274+nhuantho@users.noreply.github.com>
2025-09-20 16:25:39 +0700

    Time: 0 ms (100%), Space: 17.9 MB (45.82%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,3 +1,21 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        return math.comb(m + n - 2, m - 1)
+        dp = [[-1] * n for _ in range(m)]
+
+        def backtrack(i, j):
+            if i >= m or j >= n:
+                return 0
+
+            if i == m - 1 and j == n - 1:
+                return 1
+
+            if dp[i][j] != -1:
+                return dp[i][j]
+
+            down = backtrack(i + 1, j)
+            right = backtrack(i, j + 1)
+
+            dp[i][j] = down + right
+            return dp[i][j]
+        
+        return backtrack(0, 0)
