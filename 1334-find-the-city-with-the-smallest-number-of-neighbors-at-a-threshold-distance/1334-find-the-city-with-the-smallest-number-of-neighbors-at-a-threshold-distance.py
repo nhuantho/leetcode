68cba2be062f288455f60c7a6b84038fc734d0c2
nhuantho <91556274+nhuantho@users.noreply.github.com>
2026-04-29 10:18:46 +0700

    Time: 171 ms (70.4%), Space: 20.1 MB (96.47%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -1,28 +1,29 @@
 class Solution:
     def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
+        def floyd_warshall():
+            inf = 99999999
+            matrix = [[inf] * n for _ in range(n)]
 
-        def bellman_ford(start):
-            dist = [99999999] * n
-            dist[start] = 0
-            for _ in range(n - 1):
-                temp = dist.copy()
-                for f, t, p in edges:
-                    if dist[f] != 99999999:
-                        temp[t] = min(temp[t], dist[f] + p)
-                    if temp[t] != 99999999:
-                        temp[f] = min(temp[f], dist[t] + p)
+            for i in range(n):
+                matrix[i][i] = 0
 
-                dist = temp
-            return dist
+            for f, t, p in edges:
+                matrix[f][t] = p
+                matrix[t][f] = p
 
+            for k in range(n):
+                for i in range(n):
+                    for j in range(n):
+                        if matrix[i][j] > matrix[i][k] + matrix[k][j]:
+                            matrix[i][j] = matrix[i][k] + matrix[k][j]
+            return matrix
+
+        matrix = floyd_warshall()
         result_city = -1
         min_under_threshold = 99999999
         for i in range(n):
-            dists = bellman_ford(i)
-            under_threshold = sum([1 for j in range(n) if dists[j] <= distanceThreshold and j != i])
-
+            under_threshold = sum([1 for j in range(n) if matrix[i][j] <= distanceThreshold and i != j])
             if under_threshold <= min_under_threshold:
                 min_under_threshold = under_threshold
                 result_city = i
-
         return result_city
