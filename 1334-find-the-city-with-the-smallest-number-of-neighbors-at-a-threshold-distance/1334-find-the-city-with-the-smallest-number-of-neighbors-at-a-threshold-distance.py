729ce873c5b03270098616d2a38cbd9b3c53542d
nhuantho <91556274+nhuantho@users.noreply.github.com>
2026-04-29 10:02:05 +0700

    Time: 6934 ms (5.09%), Space: 20 MB (98.61%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -1,36 +1,24 @@
 class Solution:
     def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
-        graph = defaultdict(list)
-        for f, t, p in edges:
-            graph[f].append((t, p))
-            graph[t].append((f, p))
 
-        def dijkstra(start):
+        def bellman_ford(start):
             dist = [99999999] * n
             dist[start] = 0
-            heap = [(0, start)]
+            for _ in range(n - 1):
+                temp = dist.copy()
+                for f, t, p in edges:
+                    if dist[f] != 99999999:
+                        temp[t] = min(temp[t], dist[f] + p)
+                    if temp[t] != 99999999:
+                        temp[f] = min(temp[f], dist[t] + p)
 
-            while heap:
-                curr_dist, node = heapq.heappop(heap)
-
-                if curr_dist > dist[node]:
-                    continue
-
-                if curr_dist > distanceThreshold:
-                    continue
-
-                for neighbor, p in graph[node]:
-                    new_dist = curr_dist + p
-                    if new_dist < dist[neighbor]:
-                        dist[neighbor] = new_dist
-                        heapq.heappush(heap, (new_dist, neighbor))
-
+                dist = temp
             return dist
 
         result_city = -1
         min_under_threshold = 99999999
         for i in range(n):
-            dists = dijkstra(i)
+            dists = bellman_ford(i)
             under_threshold = sum([1 for j in range(n) if dists[j] <= distanceThreshold and j != i])
 
             if under_threshold <= min_under_threshold:
