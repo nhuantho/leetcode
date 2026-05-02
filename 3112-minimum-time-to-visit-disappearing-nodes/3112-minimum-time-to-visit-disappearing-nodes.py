6cd52a9fc64df796768865b4a76bc890d8d89a15
nhuantho <91556274+nhuantho@users.noreply.github.com>
2026-05-02 23:12:15 +0700

    Time: 382 ms (41.13%), Space: 71.7 MB (63.2%) - LeetHub

---
+++ 3112-minimum-time-to-visit-disappearing-nodes/3112-minimum-time-to-visit-disappearing-nodes.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minimumTime(self, n: int, edges: List[List[int]], disappear: List[int]) -> List[int]:
+        INF = 10 ** 9
+        graph = defaultdict(list)
+        for f, t, w in edges:
+            graph[f].append((t, w))
+            graph[t].append((f, w))
+
+        dist = [INF] * n
+        dist[0] = 0
+        heap = [(0, 0)]
+        while heap:
+            curr_w, node = heapq.heappop(heap)
+            if curr_w > dist[node]:
+                continue
+            
+            if curr_w >= disappear[node]:
+                continue
+
+            for nei, w in graph[node]:
+                new_w = curr_w + w
+                if new_w <= disappear[nei] and new_w < dist[nei]:
+                    dist[nei] = new_w
+                    heapq.heappush(heap, (new_w, nei))
+
+        results = []
+
+        for w_d, w in zip(disappear, dist):
+            if w < w_d:
+                results.append(w)
+            else:
+                results.append(-1)
+        return results
