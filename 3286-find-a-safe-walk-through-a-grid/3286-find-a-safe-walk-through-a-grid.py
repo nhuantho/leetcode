246d28d18c2babe483d9d36e10af68e545dc8fae
nhuantho <91556274+nhuantho@users.noreply.github.com>
2026-05-03 15:29:32 +0700

    Time: 75 ms (70.59%), Space: 19.6 MB (62.75%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.py
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.py
@@ -3,29 +3,28 @@ class Solution:
         INF = 10 ** 9
         m, n = len(grid), len(grid[0])
 
-        graph = defaultdict(list)
-        for i in range(m):
-            for j in range(n):
-                if i + 1 < m:
-                    graph[(i, j)].append(((i + 1, j), grid[i + 1][j]))
-                    graph[(i + 1, j)].append(((i, j), grid[i][j]))
-                if j + 1 < n:
-                    graph[(i, j)].append(((i, j + 1), grid[i][j + 1]))
-                    graph[(i, j + 1)].append(((i, j), grid[i][j]))
-
-        dist = { (i, j): INF for i in range(m) for j in range(n) }
-        dist[(0, 0)] = grid[0][0]
+        dist = [[INF] * n for _ in range(m)]
+        dist[0][0] = grid[0][0]
         heap = [(grid[0][0], (0, 0))]
+        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         while heap:
             curr_w, node = heapq.heappop(heap)
-            if curr_w > dist[node]:
-                continue
+
             if node == (m - 1, n - 1):
                 return curr_w < health
 
-            for nei, w in graph[node]:
-                new_w = curr_w + w
-                if new_w < dist[nei]:
-                    dist[nei] = new_w
-                    heapq.heappush(heap, (new_w, nei))
+            if curr_w > dist[node[0]][node[1]]:
+                continue
+
+            for d in dirs:
+                new_x = node[0] + d[0]
+                new_y = node[1] + d[1]
+                if  0 <= new_x < m and 0 <= new_y < n:
+                    new_w = curr_w + grid[new_x][new_y]
+                    if new_w > health:
+                        continue
+                    if new_w < dist[new_x][new_y]:
+                        dist[new_x][new_y] = new_w
+                        heapq.heappush(heap, (new_w, (new_x, new_y)))
+
         return False
