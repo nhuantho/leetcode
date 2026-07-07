516bf37008dfd5a87ee120a7647c7ee42a3339e4
nhuantho <91556274+nhuantho@users.noreply.github.com>
2026-07-07 14:33:58 +0700

    Time: 130 ms (24.36%), Space: 24.6 MB (32.62%) - LeetHub

---
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -0,0 +1,26 @@
+class Solution:
+    def equalPairs(self, grid: List[List[int]]) -> int:
+        d_r, d_c = dict(), dict()
+        n = len(grid)
+        for i in range(n):
+            tmp_v = ""
+            for j in range(n):
+                tmp_v += (str(grid[i][j]) + ",")
+            d_r[i] = tmp_v
+        
+        for i in range(n):
+            tmp_v = ""
+            for j in range(n):
+                tmp_v += (str(grid[j][i]) + ",")
+            d_c[i] = tmp_v
+        
+        print(d_r)
+        print(d_c)
+        
+        res = 0
+        for i in range(n):
+            for j in range(n):
+                if d_r[i] == d_c[j]:
+                    res += 1
+        return res
+
